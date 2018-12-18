9cba093dd7b903cdeb4d9039f4f2ea1f512556fd
伊賀理心 <toshimiiga@net91-dhcp-234.sfc.keio.ac.jp>
2018-12-18 11:56:15 +0900

    12/18の授業

--- Final.js
+++ Final.js
@@ -18,7 +18,6 @@ function check_answer(){
   }
 }
 
-
 function make_bg(){
     var answer = [];
 //  function make_answer(){
@@ -108,19 +107,32 @@ function your_syozoku(){
   var answer6 = document.getElementById("nameform").value;
 }
 
+
 function onFileLoaded(e){
   var src_data = e.target.result;
   img.onload = onImageSetted;
   img.src = src_data;
 }
 
+
 var img = new Image();
 
 function onFileSelected(){
   var file = document.getElementById('input_file').files[0];
   var reader = new FileReader();
-  reader.onload = onFileLoaded;
   reader.readAsDataURL(file);
+  reader.onload = function(){
+    showImage(reader.result);
+  };
+}
+
+function showImage(src){
+  var img = new Image();
+  img.src = src;
+  img.onload = function(){
+    var ctx = document.getElementById('test_canvas').getContext('2d');
+    ctx.drawImage(img, 0, 0, 256, 256);
+  };
 }
 
 function onImageSetted(e){
@@ -137,6 +149,22 @@ function createImageData(img){
   cv.height = img.naturalHeight;
   var ct = cv.getContext('2d');
   ct.drawImage(img, 0, 0);
-  var data = ct.getImageData(0, 0, cv.width, cv.height);
+  var data = ct.getImageData(0, 0, 256, 256);
   return data;
+}
+
+async function predict(){
+  var img_data = createImageData(img);
+  const model = await tf.loadModel('https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json');
+  input = tf.fromPixels(img_data);
+  input = tf.image.resizeBilinear(input, [224, 224]);
+  input = tf.expandDims(input,0);
+  input = tf.div(input, tf.scalar(255));
+  const pred = model.predict(input);
+
+  pred_idx = tf.argMax(pred, 1);
+  pred_idx = pred_idx.dataSync();
+  pred_idx = Array.from(pred_idx);
+  pred_name = classes[pred_idx[0]];
+  document.getElementById('result').innerHTML = 'Prediction* ' + pred_name;
 }
