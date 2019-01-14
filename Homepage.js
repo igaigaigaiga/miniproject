066783fc2564dad6225b13e6cafb551fdcd4d161
伊賀理心 <toshimiiga@igarishinnoMacBook-puro.local>
2019-01-14 23:18:51 +0900

    全てのcss完了

--- Homepage.js
+++ Homepage.js
@@ -6,14 +6,3 @@ function hello_name(){
   var name = document.getElementById("nameform").value;
   alert( name + "さん、こんにちは");
 }
-
-function check_answer(){
-  var answer = document.getElementById('answer').value;
-    if(answer == "とっち"){
-      alert("あなたは私の知り合いであることが確認できました！私のLINEは@fake_acountです！");
-    }else if( answer == "まっちゃん"){
-      alert("あなたはこましゅうであることが確認できました！今すぐLINEして！");
-    }else{
-      alert("不正解！");
-  }
-}
